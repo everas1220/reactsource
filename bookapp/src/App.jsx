1e7df96702a86bedef5c2a851b1ebb0064c0d867
shin seung woo <ssw030209@gmail.com>
2025-05-27 18:03:23 +0900

    jsx 웹페이지 예제

--- bookapp/src/App.jsx
+++ bookapp/src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import AddBook from './pages/AddBook'
+import EditBook from './pages/EditBook'
+import BookDetail from './components/BookDetail'
+import Error from './components/Error'
+import Home from './pages/Home'
+import Layout from './components/Layout'
+// http://localhost:5173/ => Home
+// http://localhost:5173/add => AddBook
+// http://localhost:5173/edit/1 EditBook
+// http://localhost:5173/books/1 BookDetails
+// 잘못된 경로 => Error
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<AddBook />} />
+          <Route path="/edit/:id" element={<EditBook />} />
+          <Route path="/books/:id" element={<BookDetail />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Layout>
     </>
   )
 }
--- novels/src/main/java/demo/example/novels/entity/Genre.java
+++
@@ -1,7 +0,0 @@
-package demo.example.novels.entity;
-
-public class Genre {
-    private Long id;
-
-    private String name; // 장르명
-}
